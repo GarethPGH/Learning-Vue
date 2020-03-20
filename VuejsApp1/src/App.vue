0d2a896adcd2e31ba5cb68f43143e3a98e4d79a8
GarethPGH <garethlwarren@gmail.com>
2020-03-20 15:41:59 -0400

    A little miffed
    Downloaded Visual Studio 2019 due to Vuejs support
    Restarted project, following a tutorial, TO THE LETTER
    Build Error
    Updated Visual Studio
    Build Error
    Found out a file is somehow misplaced from the autogenerated boilerplate code
    Replaced the path in the .njsproj
    Build Error
    
    Really annoyed, and worried I am never going to actually be able to CODE anything that works enough to be a published project, due to having to troubleshoot config, build and runtime errors in NEARLY EVERY PROJECT I have ever done. Worried that I am never going to actually become employable as a software developer, and due to being out of a job due to coronavirus, anxious over my income. I know this is TMI for the general public, but no one is following this repo anyway

---
+++ VuejsApp1/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="app">
+        <Todos v-bind:todos="todos"/>    
+    </div>
+</template>
+
+<script>
+    import Todos from "@/components/Todos.vue";
+
+    export default {
+        name: 'app',
+        components: {
+            Todos
+        },
+        data() {
+            return {
+                todos:[
+                    {
+                        id: 1,
+                        title: "Title 1",
+                        completed: true
+                    },
+                    {
+                        id: 2,
+                        title: "Title 2",
+                        completed: true
+                    },
+                    {
+                        id: 3,
+                        title: "Title 3",
+                        completed: false
+                    }
+                ]
+            }
+        }
+    };
+</script>
+
+<style>
+    *{
+        box-sizing: border-box;
+        margin:0;
+        padding:0;
+    }
+
+    body{
+        font-family:Arial, Helvetica, sans-serif;
+        line-height:1.4;
+    }
+    h1{color:black;}
+</style>
+
---
+++ VuejsApp1/src/components/todos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div v-for="todo in todos"></div>
+        <h3>{{todo.title}}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Todos",
+        props:["todos"]
+    }
+</script>
+
+<style scoped>
+</style>
