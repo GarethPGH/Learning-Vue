288f53d1281368d347fe8a241555b3ee50b1672f
GarethPGH <garethlwarren@gmail.com>
2020-04-09 15:38:52 -0400

    Massive changes
    Status of this project is in the readme. I have done a lot of work on it and don't want to include those changes in the commit statement.

--- VuejsApp1/src/App.vue
+++ VuejsApp1/src/App.vue
@@ -1,37 +1,67 @@
 <template>
     <div id="app">
-        <Todos v-bind:todos="todos"/>    
+        <Header/>
+        <AddTodo v-on:add-todo="addTodo"/>
+        <Todos v-bind:todos="Todos" v-on:del-todo ="deleteTodo"/>    
     </div>
 </template>
 
 <script>
-    import Todos from "@/components/Todos.vue";
+    import Header from './components/layout/Header.vue';
+    import Todos from "./components/Todos.vue";
+    import AddTodo from "./components/AddTodo.vue";
+    import axios from "axios";
+
+//Todo is referenced on render but not reactive
 
     export default {
         name: 'app',
+
         components: {
-            Todos
+            Header: Header,
+            Todos: Todos,
+            AddTodo: AddTodo
         },
         data() {
             return {
                 todos:[
-                    {
-                        id: 1,
-                        title: "Title 1",
-                        completed: true
-                    },
-                    {
-                        id: 2,
-                        title: "Title 2",
-                        completed: true
-                    },
-                    {
-                        id: 3,
-                        title: "Title 3",
-                        completed: false
-                    }
+                    {id:1,
+                    title:"Title1",
+                    completed:false},
+                
+                    {id:2,
+                    title:"Title2",
+                    completed:false},
+
+                    {id:3,
+                    title:"Title3",
+                    completed:false}
                 ]
             }
+        },
+        methods: {
+            created(){
+                axios.get('http://jsonplaceholder.typicode.com/todos?_limit=10')
+                    .then(res => this.todos = res.data)
+                    .catch(err => console.log(err));
+            },
+    
+            addTodo(newTodo){
+                const {title, completed} = newTodo;
+                axios.post('http://jsonplaceholder.typicode.com/todos', {
+                    title: title,
+                    completed: completed
+                })
+                .then(res => this.todos=[...this.todos, res.data])
+                .catch(err => console.log(err));
+            },
+            
+            deleteTodo(id){
+                axios.delete(`http://jsonplaceholder.typicode.com/todos${id}`)
+                .then(res => this.todos = this.todos.filter(res.data.id !== id) )
+                .catch(err => console.log(err));
+            }
+
         }
     };
 </script>
@@ -42,11 +72,19 @@
         margin:0;
         padding:0;
     }
-
     body{
         font-family:Arial, Helvetica, sans-serif;
         line-height:1.4;
     }
-    h1{color:black;}
+    .btn{
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
+    .btn:hover{
+        background:#666;
+    }
 </style>
-
--- VuejsApp1/src/components/todos.vue
+++ VuejsApp1/src/components/todos.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
-        <div v-for="todo in todos"></div>
-        <h3>{{todo.title}}</h3>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+        <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+        </div>
     </div>
 </template>
 
 <script>
+    import TodoItem from './TodoItem.vue';
+
     export default {
         name: "Todos",
+
+        components: {
+            TodoItem: TodoItem
+        },
+
         props:["todos"]
     }
 </script>
