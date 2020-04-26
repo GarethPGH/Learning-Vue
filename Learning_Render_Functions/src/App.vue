50083baad8246f3b309d9e865b93418ba255a9f7
GarethPGH <garethlwarren@gmail.com>
2020-04-25 20:42:28 -0400

    Learning Render Functions
    Currently rewriting the Hello World vue app example to be built entirely using render functions, to learn how to use them.
    
    Only Including the src folder with my code this time.
    
    Hello World component is in progress.
    Main page builds successfully without it, though the logo won't show up like it is supposed to

---
+++ Learning_Render_Functions/src/App.vue
@@ -0,0 +1,60 @@
+<!--turn me into a render function
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+-->
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+
+//LOOKUP THIS PAGE https://vuejs.org/v2/guide/render-function.html FULL SYNTAX EXAMPLE TOWARDS BOTTOM
+export default{
+  name:"App",
+
+  render: function(createElement) {
+    return createElement(
+      //element
+      'div',
+      //element data object
+      {
+        attrs:{id:"app"}
+      },
+      //child nodes created with createElement in an array
+      [
+        createElement(
+          'img',{
+            attrs:{
+              src: "./assets/logo.png",
+              height: "200px",
+              width: "200px",
+              alt: "Vue logo",
+            }
+        }),
+      
+        createElement(
+          'HelloWorld'
+        )
+      ]
+    )},
+
+  components:{
+    //HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Learning_Render_Functions/src/components/HelloWorld.vue
@@ -0,0 +1,107 @@
+<!-- turn me into a render function
+
+<template>
+  <div class="hello">
+  
+    <h1>{{ msg }}</h1>
+
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href= target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+
+    <h3>Installed CLI Plugins</h3>
+
+    <ul>
+      <li><a href= target="_blank" rel="noopener">babel</a></li>
+      <li><a href= target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+
+    <h3>Essential Links</h3>
+
+    <ul>
+      <li><a href= target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href= target="_blank" rel="noopener">Forum</a></li>
+      <li><a href= target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href= target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href= target="_blank" rel="noopener">News</a></li>
+    </ul>
+
+    <h3>Ecosystem</h3>
+
+    <ul>
+      <li><a href= target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href= target="_blank" rel="noopener">vuex</a></li>
+      <li><a href=target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href= target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href= target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+
+  </div>
+</template>
+-->
+<script>
+export default{
+  name: 'HelloWorld',
+
+  data: function(){
+    //links for the ul/li elements. 2-5-5
+    return{ 
+    hreferences : ["https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel","https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint",
+      "https://vuejs.org","https://forum.vuejs.org","https://chat.vuejs.org", "https://twitter.com/vuejs","https://news.vuejs.org",
+      "https://router.vuejs.org","https://vuex.vuejs.org", "https://github.com/vuejs/vue-devtools#vue-devtools","https://vue-loader.vuejs.org","https://github.com/vuejs/awesome-vue" ]
+    }
+  },
+//Get the msg from the parent
+  props: {
+    msg: String
+  },
+
+  render(createElement){
+    return createElement('div',{ class:"hello"},
+   [ 
+    createElement('h1'+ this.msg),
+    createElement('p' + "For a guide and recipes on how to configure / customize this project,check out the",
+      
+      //double nesting create element, is this a problem?
+      [createElement('a'+ "vue-cli documentation",
+        {
+          attrs:{
+            href:"https://cli.vuejs.org",
+            target: "_blank",
+            rel: "noopener"
+          } 
+        }
+      )]    
+    ),
+    createElement('h3', "Installed CLI Plugins"),
+    createElement('ul', [
+        for(let i=0;i<2,i++){  
+          createElement('li',[
+          createElement('a', {attrs:{target: '_blank', rel:'noopener',href: hreferences[i]}})]
+        })
+    ])
+   ]
+   )
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
