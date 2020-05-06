342c529ec6cce73b994cfe21f00d4db1da0ad057
GarethPGH <garethlwarren@gmail.com>
2020-05-06 10:08:36 -0400

    Updated Images.vue
    working on the thumbnails part of the gallery

--- VueGallery/src/components/Images.vue
+++ VueGallery/src/components/Images.vue
@@ -1,9 +1,13 @@
 <template>
-    
+    <div> 
+        <span v-for="image in images" v-bind:key="image.id">
+            <img :class="normal" v-bind:src="image" @onclick="showFullSize"/>
+        </span>
+    </div>
 </template>
 <script>
 import AddImage from "AddImage.vue";
-import {} from "vuex";
+import {mapGetters} from "vuex";
 export default {
     name: Images,
     //this might end up being a part of the vuex store instead, 
@@ -11,18 +15,41 @@ export default {
     //and how they should be displayed on page
     data(){
         return{
-        
+            class: normal,
+            images: this.$store.mapGetters(images)
+        }
+    },
+    //I dont know if this is redundant
+    computed:{
+        images(){
+            return this.$store.state.images;
         }
     },
 
-    computed:{
-        Images(){
-            return this.$store.state.Images;
+    methods:{
+        showFullSize:()=>{
+            if(this.class === "normal"){
+                this.class ="clicked"
+            }else{
+                this.class = "normal";
+            }
         }
     }
 }
 </script>
 
 <style scoped>
-
+.normal {
+    display: flex;
+    max-width: 25%;
+    max-height: 25%;
+    margin: 5em;
+    border-radius: 1em;
+    border: 2em, solid, rgb(25, 65, 141);
+}
+.clicked{
+    display: block;
+    width: auto;
+    height: auto;
+}
 </style>
