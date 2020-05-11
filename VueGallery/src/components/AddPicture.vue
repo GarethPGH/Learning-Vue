ae7b8c13cec17856d5203185bd3e4dcea8cb2174
GarethPGH <garethlwarren@gmail.com>
2020-05-11 09:31:00 -0400

    More comment ramblings
    trying to figure stuff out, in AddPicture

--- VueGallery/src/components/AddPicture.vue
+++ VueGallery/src/components/AddPicture.vue
@@ -7,22 +7,35 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex';
+import {Pic, setPicture} from '../store/index.js'
 
 export default {
     name: AddPicture,
+    //This is the name of the component, why the hell is it being treated like an unused variable?
     //This will be the add an image form component
     //url and id should be set by the API call
     model:{
         title: "Image Title",
         description: "Image Description"
     },
+    computed:{
+        ...mapActions(setPicture)
+    },
     methods:{
-        addNewPicture:(e)=>{
+        addNewPicture:({dispatch}, e)=>{
             e.preventDefault;
-            let newPicture= new Picture();
+            let newPicture = new Pic();
             newPicture.title = this.title;
             newPicture.description = this.description;
-        }
+            newPicture.url="http://thisisaurl.com/picture.jpg"//Switch this with API call
+            //Actually Id and API call should happen in the Vuex Action right?
+            dispatch(setPicture, newPicture);
+        },
+       /* setPictureId(id){
+            id = parseInt(this.id)+1;//Set to the index of pictures array for now
+            //this is broken, though, possibly not needed, even for mocking
+        }*/
     }
 
 }
